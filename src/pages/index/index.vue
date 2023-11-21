e546dc668cf764fbe1723865f26fe93a4a6980e4
吃饱了不饿 <ymzcode@163.com>
2023-11-21 21:56:03 +0800

    feat: 🎸 引入sdk但是报错，开始解决

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,34 @@
 <template>
   <view class="index">
     <text>{{ msg }}</text>
+    <baijia-loading class="loading-wrapper">
+    </baijia-loading>
   </view>
 </template>
 
 <script setup>
 import {ref} from "vue";
+import {useLoad} from "@tarojs/taro";
 
 const msg = ref("Hello World");
+
+
+var util = require('../../utils.js');
+var bjy = require('../../sdk/bjy');
+
+useLoad(() => {
+  util.init()
+})
+
+
 </script>
 
 
 <script>
 definePageConfig({
   navigationBarTitleText: '首页',
+  "usingComponents": {
+    "baijia-loading": "../../sdk/component/loading/loading"
+  },
 })
 </script>
